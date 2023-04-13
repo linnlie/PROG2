f76a7315c7cc6c517213f70b934bcaba56e3c761
linnlie <113351660+linnlie@users.noreply.github.com>
2023-04-13 11:08:01 +0200

    Metoder inlagda

--- Edge.java
+++ Edge.java
@@ -1,3 +1,33 @@
 public class Edge {
-    
+    public int weight;
+    public string name;
+    private final City destination;
+
+    public Edge(City destination, String name, double weight) {
+        this.destination = Objects.requireNonNull(destination);
+        this.name = Objects.requireNonNull(name);
+        if (Double.isNaN(weight)) {
+            throw new IllegalArgumentException();
+        }
+        this.weight = weight;
+    }
+    public City getDestination(){
+        return null;
+    }
+
+    public int getWeight(){
+        return null;
+    }
+
+    public void setWeight(int distance){
+
+    }
+
+    public string getName(){
+        return null;
+    }
+
+    public string toString(){
+        return null;
+    }
 }
--- ListGraph.java
+++ ListGraph.java
@@ -1,4 +1,48 @@
 public class ListGraph {
-    
+    private final Map<City, Set<Edge>> nodes = new HashMap<>();
+
+    public void add(City city) {
+        nodes.putIfAbsent(city, new HashSet<>());
+    }
+
+    public void remove(City city){
+
+    }
+
+    public void connect(City a, City b, string name, int distance){
+
+    }
+
+    public void disconnect(City a, City b){
+
+    }
+
+    public void setConnectionWeight(City a, City b, int newDistance){
+
+    }
+
+    public HashMap<City, Set<Egde>> getNodes(){
+        return null;
+    }
+
+    public Set<Edge> getEdgesFrom(City city){
+        return null;
+    }
+
+    public Edge getEdgeBetween(City a, City b){
+        return null;
+    }
+
+    public string toString(){
+        return null;
+    }
+    public boolean pathExists(City a, City b){
+        return null;
+    }
+
+    public List<Edge> getPath(City a, City b){
+        return null;
+    }
+
 }
 
