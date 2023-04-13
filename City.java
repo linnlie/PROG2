9ccbfbdb684ea9eb79dfc36c2989bd86693b0a92
linpack12 <55255203+linpack12@users.noreply.github.com>
2023-04-13 12:25:58 +0200

    lagt till city som klass och tostring metod för edge klassen

---
+++ City.java
@@ -0,0 +1,29 @@
+import java.util.Objects;
+
+public class City{
+
+    private final String cityName;
+
+    public City(String cityName){
+        this.cityName = cityName;
+    }
+
+    public String getName(){
+        return this.cityName;
+    }
+
+    public boolean equals(Object other){
+        if(other instanceof City city){
+            return cityName.equals(city.cityName);
+        }
+        return false;
+    }
+
+    public int hashCode(){
+        return Objects.hash(cityName);
+    }
+
+    public String toString(){
+        return this.cityName;
+    }
+}
