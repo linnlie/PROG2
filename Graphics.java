db0e88b2ca56b776f6d65745cdce46b2d72b2084
linnlie <113351660+linnlie@users.noreply.github.com>
2023-05-10 09:35:35 +0200

    skrivit om linus kommentarer

--- Graphics.java
+++ Graphics.java
@@ -26,31 +26,24 @@ public class Graphics extends Application{
     @Override
     public void start(Stage primaryStage) {
 
-        //här skapa jag en lista som innehåller alla underrubriker för "file" menyn, blir mindre rader kod vilket är fint
+        //lista med underrubriker för "file" menyn
         List<String> menuNamnen = List.of("New Map", "Open", "Save", "Save Image", "Exit");
-        //här skapas en meny bar
         MenuBar menuBar = new MenuBar();
-        //här skapas en vbox som håller menybaren, som jag fattar det så stackar den alla childs vertikalt på varandra
         VBox vBox = new VBox(menuBar);
 
-        //här skapar jag en ny flowpane som håller alla ui element
         FlowPane buttonPane = new FlowPane();
-        //här sätter jag mellanrummet/spacing mellan ui elementen i detta fall knapparna både vertikalt och horizontelt
+        //sätter mellanrum/spacing mellan ui elementen (knapparna) både vertikalt och horisontellt
         buttonPane.setHgap(20);
         buttonPane.setVgap(20);
 
         //här sätter jag den "önskade" bredden på flowpanen
         buttonPane.setPrefWrapLength(800);
-        //här lägger jag till flowpanen till vboxen
+
         vBox.getChildren().add(buttonPane);
-
-        //sätter scenen till stage
-        //här sätter jag texten som står högst uppe i vänstra hörnet på uppgiftsbeskrivningen, lite osäker på om det behövs vara exakt samma men aja
-
 
         Menu fileMenu = new Menu("File"); //Skapar & sätter Meny-texten till File
 
-        //Skapar å lägger till MenuItems till fileMenu, precis som ni hade gjort innan men med hjälp av listan där uppe och en for loop
+        //loopar igenom listan av menynamnen, skapar en menuitem för varje namn och lägger till eventhanterare via handlefilemenu + lägger till i filemenyn
         for(String rubrikNamn : menuNamnen){
             MenuItem menuItem = new MenuItem(rubrikNamn);
             menuItem.setOnAction(this::handleFileMenyn);
@@ -64,7 +57,7 @@ public class Graphics extends Application{
         primaryStage.setWidth(1000);
         primaryStage.setHeight(400);
 
-        //här skapade jag en lista med knapper för insåg hur fult de kommer bli add behöva addera alla knapper separat till pane
+        //lista med meny knappar
         List<CustomButton> buttons = List.of(
                 new CustomButton("Find Path", 0, 0),
                 new CustomButton("Show Connection", 0, 0),
@@ -73,9 +66,9 @@ public class Graphics extends Application{
                 new CustomButton("Change Connection", 0, 0)
         );
 
-        buttonPane.getChildren().addAll(buttons); //lägger in knapperna i pane
+        buttonPane.getChildren().addAll(buttons); //lägger in knapparna i pane
 
-        //här adderar jag en eventhandlar till varje separat knapp som är tillagd i listan, så man får feedback på att de funkar, också najs med lista här för annars hade man behövt addera en ny eventhandlar till varje knapp vilket blir snuskigt
+        //här adderar jag en eventhandlar till varje separat knapp som är tillagd i listan
         for (CustomButton button : buttons) {
             button.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
@@ -84,25 +77,21 @@ public class Graphics extends Application{
                 }
             });
         }
-        //skapar en egen pane för själva bilden så att den inte lagras på knapparna (finns säkert något bättre sätt man kan göra det aka kolla i Linns anteckningar kanske)
+
+        //skapar en egen pane för själva bilden så att den inte lagras på knapparna
         VBox imagePane = new VBox();
-
-        //skapar ramen för bilden vi ska ladda
-        imageView = new ImageView();
-
-        //adderar ramen till bildens pane
-        imagePane.getChildren().add(imageView);
+        imageView = new ImageView(); //ramen för bilden
+        imagePane.getChildren().add(imageView); //lägger till ramen till bildens pane
 
         //skapar en parent pane som håller menupanen/file menyn, pane för knapparna, och bilden som ska laddas
         VBox root = new VBox();
         root.getChildren().addAll(vBox, buttonPane, imagePane);
+
         //här skapas en scene med vbox som root och måtten/storleken
         Scene scene = new Scene(root, 960, 600);
         primaryStage.setScene(scene);
-        //visar fönstret/stage
         primaryStage.show();
         primaryStage.setTitle("PathFinder");
-
     }
 
     public void handleFileMenyn(ActionEvent event){
@@ -112,7 +101,6 @@ public class Graphics extends Application{
         switch(menuNamn){
             case "New Map":
                 if (!imageLoaded) {
-                    //vet inte om de är så mycket att förklara men
                     String imageUrl = "europa.gif";
                     Image image = new Image(imageUrl);
                     imageView.setImage(image);
