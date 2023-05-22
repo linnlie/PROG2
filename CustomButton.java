568df96a0b2438ed2b01751b0353fc3cf76e907d
linpack12 <55255203+linpack12@users.noreply.github.com>
2023-05-22 19:42:04 +0200

    hitta fel på custombuttons

--- CustomButton.java
+++ CustomButton.java
@@ -1,18 +1,14 @@
-import java.awt.Button;
+import javafx.scene.control.Button;
 
-
-public class CustomButton extends Button {
-
-    //konstuktor som tar en texten på knappen och x samt y kordinaten i pane
-    public CustomButton(String text, double x, double y) {
-        super(text); //anropa superklassens konstruktor för att sätta texten på knappen
-        //sätter x och y kordinaterna i pane för knappen
-        //setLayoutY(y);
-        //setLayoutX(x);
+public class CustomButton extends Button{
+    
+    public CustomButton(String text, double x, double y){
+        super(text);
+        setLayoutX(x);
+        setLayoutY(y);
     }
 
-    //metod för att ändra texten på knappen om man vill göra det :)
-    public void setButtonText(String text) {
-        super.setLabel(text); //anropa superklassens setText metoden för att sätta texten på knappen
+    public void setButtonText(String text){
+        super.setText(text);
     }
 }
